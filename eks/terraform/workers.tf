41af3499fe8bf58f5f1bf45ca7eaa89db777c32b
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2021-06-30 15:33:34 +0100

    increase the size of the worker node
    I suspect there is some resource exhaustion since often the worker node get into unreachable state and also there are some pods eviction

--- eks/terraform/workers.tf
+++ eks/terraform/workers.tf
@@ -35,6 +35,8 @@ resource "aws_eks_node_group" "k8s-fred-node-group" {
   node_role_arn   = aws_iam_role.k8s-fred-worker-role.arn
   subnet_ids      = data.terraform_remote_state.infra_networking.outputs.private_subnet_ids
 
+  instance_types  = ["m5.4xlarge"]
+
   scaling_config {
     desired_size = 1
     max_size     = 1
